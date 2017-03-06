3a12f9755b9610ca7e2e3c1d7ef8e216420781c3
Erick Simões <erick.simoes@live.com>
2017-03-06 19:13:34 -0300

    feat: Habilitado o modo fullscrean

--- passa_ou_repassa.py
+++ passa_ou_repassa.py
@@ -44,6 +44,11 @@ root = Tk()
 root.title = "Makerama"
 root.configure(background='white')
 
+#fullscrean
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+
 #set focus
 root.focus_set() # <-- move focus to this widget
 
